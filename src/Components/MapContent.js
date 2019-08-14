59feac58ab331674a8035240a6b0f382ee15545d
eozturk <eozturk@microstrategy.com>
2019-08-14 20:19:13 +0200

    Deneme

--- src/Components/MapContent.js
+++ src/Components/MapContent.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import isEmpty from 'lodash.isempty';
 import GoogleMapReact from 'google-map-react';
-import { Layout, Divider } from 'antd';
+import {Layout, Divider} from 'antd';
 
 const API_LINK = 'https://api.citybik.es/v2/networks/veturilo-warszawa';
 const markers = [];
 const infowindows = [];
-const { Content } = Layout;
+const {Content} = Layout;
 
 export default class MapComponent extends Component {
 
@@ -16,7 +16,7 @@ export default class MapComponent extends Component {
       places: [],
     };
   }
-
+  //////////////////////
   static defaultProps = {
     center: {
       lat: 52.229676,
@@ -98,7 +98,7 @@ export default class MapComponent extends Component {
   };
 
   render() {
-    const { places } = this.state;
+    const {places} = this.state;
 
     return (
       <Layout>
@@ -109,9 +109,9 @@ export default class MapComponent extends Component {
               <GoogleMapReact
                 defaultZoom={this.props.zoom}
                 defaultCenter={this.props.center}
-                bootstrapURLKeys={{ key: 'AIzaSyBezTSjZSagkNk4MhbEy2CKJIRtPmAS2BE' }}
+                bootstrapURLKeys={{key: 'AIzaSyBezTSjZSagkNk4MhbEy2CKJIRtPmAS2BE'}}
                 yesIWantToUseGoogleMapApiInternals
-                onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps, places)}
+                onGoogleApiLoaded={({map, maps}) => this.handleApiLoaded(map, maps, places)}
               />
             )}
           </Content>
@@ -119,13 +119,13 @@ export default class MapComponent extends Component {
             {
               places.map((place, i) => <Content key={i} className="PlacesContent">
                 <Content id={place.id} className="PlacesInnerCell"
-                  onMouseOver={({ map, maps }) => this.highlightPlaceFromTheList(map, maps, places, place)}
-                  onMouseOut={({ map, maps }) => this.closeInfowindow(map, maps, places, place)}>
+                  onMouseOver={({map, maps}) => this.highlightPlaceFromTheList(map, maps, places, place)}
+                  onMouseOut={({map, maps}) => this.closeInfowindow(map, maps, places, place)}>
                   <Content className="CntPlaceName">{place.name}</Content>
                   <Content className="CntFrame">
                     <Content className="CntInfos">Total slots : {place.extra.slots}</Content>
-                    <Content className="CntInfos" >Empty slots : <span style={{ color: place.empty_slots > 5 ? 'green' : 'red' }}>{place.empty_slots}</span></Content>
-                    <Content className="CntInfos" style={{ padding: place.empty_slots < 10 ? '0 0 0 10px' : '0 0 0 5px' }}>Free Bikes  : <span style={{ color: place.free_bikes > 5 ? 'green' : 'red' }}>{place.free_bikes}</span></Content>
+                    <Content className="CntInfos" >Empty slots : <span style={{color: place.empty_slots > 5 ? 'green' : 'red'}}>{place.empty_slots}</span></Content>
+                    <Content className="CntInfos" style={{padding: place.empty_slots < 10 ? '0 0 0 10px' : '0 0 0 5px'}}>Free Bikes  : <span style={{color: place.free_bikes > 5 ? 'green' : 'red'}}>{place.free_bikes}</span></Content>
                   </Content>
                 </Content>
               </Content>)
